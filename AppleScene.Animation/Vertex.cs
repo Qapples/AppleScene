f44b99083b0e8d1f7fb4ce2bcfc1b64075a6042b
qapples <793678@lcps.org>
2021-06-23 13:13:28 -0400

    Add Tuple constructor for Vertex.cs

--- AppleScene.Animation/Vertex.cs
+++ AppleScene.Animation/Vertex.cs
@@ -33,5 +33,15 @@ namespace AppleScene.Animation
         /// <param name="weights">How much this vertex is affected by the joints.</param>
         public Vertex(in VertexPositionNormalTexture vertexData, JointVector joints, Vector4 weights) =>
             (VertexData, Joints, Weights) = (vertexData, joints, weights);
+
+        /// <summary>
+        /// Creates an instance of Vertex (with a tuple of Joints instead of JointVector)
+        /// </summary>
+        /// <param name="vertexData">Defines the data of the vertex such as position, normal, and texture cords.</param>
+        /// <param name="joints">The four Joints that have an affect on this vertex.</param>
+        /// <param name="weights">How much this vertex is affected by the joints.</param>
+        public Vertex(in VertexPositionNormalTexture vertexData, (Joint x, Joint y, Joint z, Joint w) joints,
+            Vector4 weights) => (VertexData, Joints, Weights) =
+            (vertexData, new JointVector(joints.x, joints.y, joints.z, joints.w), weights);
     }
 }
