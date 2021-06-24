431788982d9eeb208c0c97f7b50b60af3e6c4c63
qapples <793678@lcps.org>
2021-06-23 20:02:59 -0400

    Change Joint.cs to refer to an array.

--- AppleScene.Animation/Joint.cs
+++ AppleScene.Animation/Joint.cs
@@ -11,26 +11,40 @@ namespace AppleScene.Animation
         public Node Node { get; set; }
 
         /// <summary>
-        /// Matrix that represents the position and orientation of the joint. Used to calculate the skin matrix for the
-        /// vertices.
+        /// Refers to a matrix in a jointMatrices array. This matrix that represents the position and orientation of the
+        /// joint. Used to calculate the skin matrix for the vertices.
         /// </summary>
-        public Matrix JointMatrix { get; set; }
+        public int JointMatrixIndex { get; set; }
+
+        /// <summary>
+        /// Returns a new matrix instance that is a copy of the matrix found in the provided jointMatrices at the
+        /// JointMatrixIndex
+        /// </summary>
+        public Matrix JointMatrix => _jointMatrices[JointMatrixIndex];
         
         /// <summary>
         /// The inverse of the global transform when the bone is at it's bind (or default) position. 
         /// </summary>
         public Matrix InverseBindTransform { get; set; }
 
+
+        private Matrix[] _jointMatrices;
+
         /// <summary>
         /// Creates an instance of Joint.
         /// </summary>
         /// <param name="node">The node the joint is attached to.</param>
+        /// <param name="jointMatrixIndex"></param>
         /// <param name="globalTransformOfMesh">The global transform of the mesh. Used to calculate the JointMatrix
         /// property.</param>
         /// <param name="inverseBindTransform">The inverse bind transform of the joint.</param>
-        public Joint(Node node, in Matrix globalTransformOfMesh, in Matrix inverseBindTransform) =>
-            (Node, JointMatrix, InverseBindTransform) = (node,
-                Matrix.Invert(globalTransformOfMesh) * node.WorldMatrix * inverseBindTransform, inverseBindTransform);
+        /// <param name="jointMatrices"></param>
+        public Joint(Node node, Matrix[] jointMatrices, int jointMatrixIndex, in Matrix globalTransformOfMesh,
+            in Matrix inverseBindTransform)
+        {
+            (Node, _jointMatrices, JointMatrixIndex, jointMatrices[jointMatrixIndex], InverseBindTransform) = 
+                (node, jointMatrices, jointMatrixIndex, Matrix.Invert(globalTransformOfMesh) * node.WorldMatrix * inverseBindTransform, inverseBindTransform);
+        }
     }
 
     public record JointVector(Joint X, Joint Y, Joint Z, Joint W);
