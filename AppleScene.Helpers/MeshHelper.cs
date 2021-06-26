3adab4af63def83eb466dab69dccc9695280c138
qapples <793678@lcps.org>
2021-06-25 21:47:20 -0400

    Add GetVertexType for MeshHelper.cs

--- AppleScene.Helpers/MeshHelper.cs
+++ AppleScene.Helpers/MeshHelper.cs
@@ -1,3 +1,7 @@
+#nullable enable
+using System;
+using System.Diagnostics;
+using AppleScene.Animation;
 using Microsoft.Xna.Framework.Graphics;
 using SharpGLTF.Schema2;
 
@@ -9,12 +13,36 @@ namespace AppleScene.Helpers
     public static class MeshHelper
     {
         /// <summary>
-        /// Draws a mesh with a specified GraphicsDevice.
+        /// Gets the vertex type that a primitive uses for it's vertices.
         /// </summary>
-        /// <param name="mesh">The mesh to draw.</param>
-        /// <param name="graphicsDevice">GraphicsDevice instance to draw with.</param>
-        public static void Draw(this Mesh mesh, GraphicsDevice graphicsDevice)
+        /// <param name="primitive">The primitive to get the vertex type from.</param>
+        /// <returns>If successful, the vertex type of the primitive is used. If unsuccessful and the type cannot be
+        /// determined, then null is returned.</returns>
+        public static Type? GetVertexType(this MeshPrimitive primitive)
         {
+            //This mask represents what accessors the primitive has to determine what VertexType to use.
+            byte mask = (byte) (IsAccessor(primitive, "POSITION") +
+                                (IsAccessor(primitive, "NORMAL") << 1) +
+                                (IsAccessor(primitive, "TEXCOORD_0") << 2) +
+                                (IsAccessor(primitive, "JOINTS_0") << 3) +
+                                (IsAccessor(primitive, "WEIGHTS_0") << 4));
+
+            switch (mask)
+            {
+                case 0b0000_0101: //has both Position and TexCoord_0
+                    return typeof(VertexPositionTexture);
+                case 0b0000_0111: //has Position, Normal, and TexCoord_0
+                    return typeof(VertexPositionNormalTexture);
+                case 0b0001_111: //has Position, Normal, TexCoord_0, Joints_0, and Weights_0
+                    return typeof(JointVertexType);
+                default:
+                    Debug.WriteLine($"GetVertexType: Cannot find valid vertex type for primitive: {primitive}. " +
+                                    $"Byte mask: {mask}. Returning null.");
+                    return null;
+            }
         }
+
+        private static byte IsAccessor(MeshPrimitive primitive, string name) =>
+            primitive.VertexAccessors[name] is null ? (byte) 0 : (byte) 1;
     }
 }
