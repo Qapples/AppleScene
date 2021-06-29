11e58121c2349fe94b1c28ac4420d182f51a6733
qapples <793678@lcps.org>
2021-06-29 11:44:18 -0400

    Fix offset in JointVertexType VertexDeclaration creation.

--- AppleScene.Animation/JointVertexType.cs
+++ AppleScene.Animation/JointVertexType.cs
@@ -64,14 +64,18 @@ namespace AppleScene.Animation
         {
             int offset = 0;
 
+            //This is a bit of a trick we're doing here so that we can declare a new VertexDeclaration on the spot
+            //without having to use List.Add(). The "+=" operator for int returns the value AFTER int has been
+            //incremented (like i++). So, we immediately subtract the value we added to the returned value to
+            //compensate.
             VertexDeclaration = new VertexDeclaration(
-                new VertexElement(offset += 12, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
-                new VertexElement(offset += 12, VertexElementFormat.Vector3, VertexElementUsage.Normal, 0),
-                new VertexElement(offset += 8, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
+                new VertexElement((offset += 12) - 12, VertexElementFormat.Vector3, VertexElementUsage.Position, 0),
+                new VertexElement((offset += 12) - 12, VertexElementFormat.Vector3, VertexElementUsage.Normal, 0),
+                new VertexElement((offset += 8) - 8, VertexElementFormat.Vector2, VertexElementUsage.TextureCoordinate, 0),
 
                 //BlendIndices in this case refer to the joints.
-                new VertexElement(offset += 8, VertexElementFormat.Short4, VertexElementUsage.BlendIndices, 0),
-                new VertexElement(offset += 16, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 0));
+                new VertexElement((offset += 8) - 8, VertexElementFormat.Short4, VertexElementUsage.BlendIndices, 0),
+                new VertexElement((offset += 16) + 16, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 0));
         }
     }
 }
